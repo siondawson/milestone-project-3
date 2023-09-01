2cac6c669cf01d705cb4caca0fdf1a419c7adf53
siondawson <>
2023-09-01 14:29:15 +0000

    add login html structure

--- live_music_cardiff/routes.py
+++ live_music_cardiff/routes.py
@@ -4,9 +4,14 @@ from live_music_cardiff import app, db
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("home.html")
 
 
 @app.route("/sign_up")
 def sign_up():
     return render_template("sign_up.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
