b9e7f36fdc0ebf7c3ebea44e132aca3bf84ab209
siondawson <>
2023-09-04 12:36:53 +0000

    add flash messages

--- live_music_cardiff/routes.py
+++ live_music_cardiff/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, flash
 from live_music_cardiff import app, db
 
 
@@ -9,14 +9,33 @@ def home():
 
 @app.route("/sign_up", methods=['GET', 'POST'])
 def sign_up():
-     return render_template("sign_up.html")
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password2')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be greater than 1 characters.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters', category='error')
+        else:
+            flash('Account created!', category='success')
+
+    return render_template("sign_up.html")
       
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/signin', methods=['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template("login.html", boolean=True)
 
 
-@app.route('/logout')
+@app.route('/signout')
 def logout():
-    return "<p>Logout</>"
+    return "<p>Logout</p>"
