9167b4560d962784087da499e64c486fe7b777f4
siondawson <>
2023-09-18 18:30:06 +0000

    add jquery timepicker to add_event. db functionality not yet completed

--- live_music_cardiff/static/js/script.js
+++ live_music_cardiff/static/js/script.js
@@ -5,4 +5,16 @@ $(function () {
 
 });
 
+$('.timepicker').timepicker({
+    timeFormat: 'h:mm p',
+    interval: 30,
+    minTime: '7',
+    maxTime: '6:00pm',
+    defaultTime: '11',
+    startTime: '10:00',
+    dynamic: false,
+    dropdown: true,
+    scrollbar: true
+});
+
 document.getElementById("year").innerHTML = new Date().getFullYear();
